8d98f09ee15751eebcd9779d6fbb676843188524
Agustín <agustinlizarralde@gmail.com>
2016-06-03 19:30:20 -0300

    agregadas consultas B y C

--- src/main/java/bd2/util/Queries.java
+++ src/main/java/bd2/util/Queries.java
@@ -1,5 +1,6 @@
 package bd2.util;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.hibernate.Query;
@@ -9,12 +10,11 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 
-import bd2.model.Sitio;
 
 public class Queries {
 	
 	private static SessionFactory sessions;
-
+	
 	public static void consultaItemA() {
 		Query queryResult;
 		List list;
@@ -34,6 +34,9 @@ public class Queries {
 				list = queryResult.list();
 				tx.commit();
 				System.out.println(list);
+				while( iterator.hasNext() ){
+					System.out.println("Documento: "+ iterator.next());
+				}
 			} catch (Exception e) {
 				e.printStackTrace();
 				if (tx != null) {
@@ -52,6 +55,97 @@ public class Queries {
 	
 	public static void main(String[] args) {
 		consultaItemA();
+		consultaItemB();
+		consultaItemC();
 	}
 	
+	public static void consultaItemB() {
+		Query queryResult;
+		List list;
+		try {
+			System.out.println("----------------------- Setting up Hibernate -----------------------");
+			Configuration cfg = new Configuration();
+			cfg.configure("hibernate/hibernate.cfg.xml");
+
+			System.out.println("Building sessions.........");
+			sessions = cfg.buildSessionFactory();
+
+			Session session = sessions.openSession();
+			Transaction tx = null;
+			try {
+				tx = session.beginTransaction();
+				queryResult = session.createQuery(""
+						+ "select distinct m.email "
+						+ "from Moderador as m "
+						+ "join m.evaluaciones as e "
+						+ "where e.traduccion.idioma.nombre = :idioma "
+						)
+						.setString("idioma", "Ingles");
+				list = queryResult.list();
+				tx.commit();
+				Iterator<String> iterator = list.iterator();
+				while( iterator.hasNext() ){
+					System.out.println("Email: "+ iterator.next());
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				if (tx != null) {
+					tx.rollback();
+				}
+				session.close();
+			}
+			session.disconnect();
+
+			System.out.println("DONE.");
+		} catch (Exception e) {
+			System.out.println("------------------------FAIL.------------------------");
+			e.printStackTrace();
+		}
+	}
+	
+	public static void consultaItemC() {
+		Query queryResult;
+		List list;
+		try {
+			System.out.println("----------------------- Setting up Hibernate -----------------------");
+			Configuration cfg = new Configuration();
+			cfg.configure("hibernate/hibernate.cfg.xml");
+
+			System.out.println("Building sessions.........");
+			sessions = cfg.buildSessionFactory();
+
+			Session session = sessions.openSession();
+			Transaction tx = null;
+			try {
+				tx = session.beginTransaction();
+				queryResult = session.createQuery(""
+						+ "select distinct u.nombre "
+						+ "from Usuario as u "
+						+ "join u.cursadasRealizadas as cr "
+						+ "join cr.curso as c "
+						+ "where c.idioma.nombre = :idioma "
+						+ "and c.nivel >= 3 "
+						)
+						.setString("idioma", "Frances");
+				list = queryResult.list();
+				tx.commit();
+				Iterator<String> iterator = list.iterator();
+				while( iterator.hasNext() ){
+					System.out.println("Nombre: "+ iterator.next());
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				if (tx != null) {
+					tx.rollback();
+				}
+				session.close();
+			}
+			session.disconnect();
+
+			System.out.println("DONE.");
+		} catch (Exception e) {
+			System.out.println("------------------------FAIL.------------------------");
+			e.printStackTrace();
+		}
+	}
 }
