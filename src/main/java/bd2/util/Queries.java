876927d48f345e9b8ca0bf6bf7daabe0602d8cba
Agustín <agustinlizarralde@gmail.com>
2016-06-11 17:59:30 -0300

    Agregadas consultas D y E

--- src/main/java/bd2/util/Queries.java
+++ src/main/java/bd2/util/Queries.java
@@ -1,5 +1,8 @@
 package bd2.util;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.List;
 
@@ -14,6 +17,10 @@ import org.hibernate.tool.hbm2ddl.SchemaExport;
 public class Queries {
 	
 	private static SessionFactory sessions;
+	
+	public static void main(String[] args) {
+		consultaItemE();
+	}
 	
 	public static void consultaItemA() {
 		Query queryResult;
@@ -33,6 +40,7 @@ public class Queries {
 				queryResult = session.createQuery("select nombre from Documento");
 				list = queryResult.list();
 				tx.commit();
+				Iterator<String> iterator = list.iterator();
 				System.out.println(list);
 				while( iterator.hasNext() ){
 					System.out.println("Documento: "+ iterator.next());
@@ -51,12 +59,6 @@ public class Queries {
 			System.out.println("------------------------FAIL.------------------------");
 			e.printStackTrace();
 		}
-	}
-	
-	public static void main(String[] args) {
-		consultaItemA();
-		consultaItemB();
-		consultaItemC();
 	}
 	
 	public static void consultaItemB() {
@@ -127,6 +129,100 @@ public class Queries {
 						+ "and c.nivel >= 3 "
 						)
 						.setString("idioma", "Frances");
+				list = queryResult.list();
+				tx.commit();
+				Iterator<String> iterator = list.iterator();
+				while( iterator.hasNext() ){
+					System.out.println("Nombre: "+ iterator.next());
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				if (tx != null) {
+					tx.rollback();
+				}
+				session.close();
+			}
+			session.disconnect();
+
+			System.out.println("DONE.");
+		} catch (Exception e) {
+			System.out.println("------------------------FAIL.------------------------");
+			e.printStackTrace();
+		}
+	}
+	
+	public static void consultaItemD() {
+		Query queryResult;
+		List list;
+		try {
+			System.out.println("----------------------- Setting up Hibernate -----------------------");
+			Configuration cfg = new Configuration();
+			cfg.configure("hibernate/hibernate.cfg.xml");
+
+			System.out.println("Building sessions.........");
+			sessions = cfg.buildSessionFactory();
+
+			Session session = sessions.openSession();
+			Transaction tx = null;
+			try {
+				Date desde = new GregorianCalendar(2015,Calendar.JULY,1).getTime();
+				Date hasta = new GregorianCalendar(2015,Calendar.DECEMBER,31).getTime();
+				tx = session.beginTransaction();
+				queryResult = session.createQuery(""
+						+ "select distinct m.nombre "
+						+ "from Moderador as m "
+						+ "join m.evaluaciones as e "
+						+ "where e.fecha between :desde and :hasta"
+						)
+						.setDate("desde", desde )
+						.setDate("hasta", hasta );
+				list = queryResult.list();
+				tx.commit();
+				Iterator<String> iterator = list.iterator();
+				while( iterator.hasNext() ){
+					System.out.println("Nombre: "+ iterator.next());
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				if (tx != null) {
+					tx.rollback();
+				}
+				session.close();
+			}
+			session.disconnect();
+
+			System.out.println("DONE.");
+		} catch (Exception e) {
+			System.out.println("------------------------FAIL.------------------------");
+			e.printStackTrace();
+		}
+	}
+	
+	public static void consultaItemE() {
+		Query queryResult;
+		List list;
+		try {
+			System.out.println("----------------------- Setting up Hibernate -----------------------");
+			Configuration cfg = new Configuration();
+			cfg.configure("hibernate/hibernate.cfg.xml");
+
+			System.out.println("Building sessions.........");
+			sessions = cfg.buildSessionFactory();
+
+			Session session = sessions.openSession();
+			Transaction tx = null;
+			try {
+				tx = session.beginTransaction();
+				queryResult = session.createQuery(""
+						+ "select t.descripcion "
+						+ "from Tarea as t "
+						+ "join t.idioma as idiomaTraduccion "
+						+ "join t.parrafo.documento.idioma as idiomaOriginal "
+						+ "where t.completa = true "
+						+ "and t.class = 'T' "
+						+ "and idiomaTraduccion.nombre = 'frances' "
+						+ "and idiomaOriginal.nombre = 'ingles' "
+						);
 				list = queryResult.list();
 				tx.commit();
 				Iterator<String> iterator = list.iterator();
